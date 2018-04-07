430f788ae077d494ab9a20c5a0f4a35f58c24312
vq-caleb <calebvillaq@gmail.com>
2018-04-07 11:29:44 -0600

    Perfil del lugar

---
+++ app/src/main/java/com/example/denis/a2night/PerfilLugar.java
@@ -0,0 +1,13 @@
+package com.example.denis.a2night;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PerfilLugar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_lugar);
+    }
+}
