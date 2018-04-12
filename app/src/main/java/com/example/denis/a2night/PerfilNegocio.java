d43270d8ec509b52fdd539fe5d58eeaa0bebb523
vq-caleb <calebvillaq@gmail.com>
2018-04-11 22:30:31 -0600

    Diseñando tabs perfil negocio

--- app/src/main/java/com/example/denis/a2night/PerfilNegocio.java
+++ app/src/main/java/com/example/denis/a2night/PerfilNegocio.java
@@ -1,10 +1,8 @@
 package com.example.denis.a2night;
 
-
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +11,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.denis.a2night.entidades.Empresa;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -41,6 +42,27 @@ public class PerfilNegocio extends Fragment {
                 transaction.replace(R.id.content, new Buscar()).commit();
             }
         });
+
+
+        OnclickDelTextView(R.id.info, view);
+        OnclickDelTextView(R.id.promo, view);
+
+        // alambramos el TextView
+        /*TextView MiTextView = (TextView) view.findViewById(R.id.info);
+        MiTextView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+
+            public void onClick(View arg0) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.contenido, new TabInformacion()).commit();
+            }
+
+        });*/
+
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.contenido, new TabPost()).commit();
+
         this.alambrarVariables(view);
         this.completarInformacion();
         return view;
@@ -59,4 +81,30 @@ public class PerfilNegocio extends Fragment {
         this.cantidadAsistentes =  (TextView) view.findViewById(R.id.cantidadAsistentes);
         this.etiquetaUbicacion = (TextView) view.findViewById(R.id.etiquetaUbicacion);
     }
+
+    public void OnclickDelTextView(int ref, View view) {
+
+        View view2 = view.findViewById(ref);
+        TextView miTextView = (TextView) view2;
+
+        miTextView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+
+                    case R.id.info:
+                        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                        transaction.replace(R.id.contenido, new TabInformacion()).commit();
+                        break;
+
+                    case R.id.publicacion:
+                        FragmentTransaction transaction2 = getFragmentManager().beginTransaction();
+                        transaction2.replace(R.id.contenido, new TabPost()).commit();
+
+                        break;
+                    default:break; }// fin de casos
+            }// fin del onclick
+        });
+    }// fin de OnclickDelTextView
+
 }
