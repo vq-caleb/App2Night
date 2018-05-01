38cc3dc67e0de0f7dd50df60883f40e530d71952
scalderon3010 <s.calderon3010@gmail.com>
2018-04-30 21:05:00 -0600

    Actualizando

--- app/src/main/java/com/example/denis/a2night/Perfil.java
+++ app/src/main/java/com/example/denis/a2night/Perfil.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class Perfil extends Fragment {
 
+    TextView video;
 
     public Perfil() {
         // Required empty public constructor
@@ -23,7 +26,15 @@ public class Perfil extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+        video = (TextView) view.findViewById(R.id.video);
+        video.setText("https://youtu.be/YtjOaZTiuWY");
+
+
+        return view;
+
     }
+
+
 
 }
