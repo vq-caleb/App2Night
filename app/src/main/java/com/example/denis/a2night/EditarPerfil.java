c176a7b718cf57d2b0f54c856625a6420a0afde7
Anthony <anthony.soto.cordoba@est.una.ac.cr>
2018-05-16 16:00:22 -0600

    WORKING BITCH

--- app/src/main/java/com/example/denis/a2night/EditarPerfil.java
+++ app/src/main/java/com/example/denis/a2night/EditarPerfil.java
@@ -3,9 +3,13 @@ package com.example.denis.a2night;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
@@ -23,7 +27,23 @@ public class EditarPerfil extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_editar_perfil, container, false);
+        View view = inflater.inflate(R.layout.fragment_editar_perfil, container, false);
+
+        /*OnChangeDelEditText(view.findViewById(R.id.correoElectronico));
+        OnChangeDelEditText(view.findViewById(R.id.nombre));
+        OnChangeDelEditText(view.findViewById(R.id.etiqueta));
+        OnChangeDelEditText(view.findViewById(R.id.telefono1));
+        OnChangeDelEditText(view.findViewById(R.id.telefono2));
+        OnChangeDelEditText(view.findViewById(R.id.pagFacebook));
+        OnChangeDelEditText(view.findViewById(R.id.pagInstagram));
+        OnChangeDelEditText(view.findViewById(R.id.pagTwitter));
+        OnChangeDelEditText(view.findViewById(R.id.tipo));
+        OnChangeDelEditText(view.findViewById(R.id.cover));
+        OnChangeDelEditText(view.findViewById(R.id.codVestimenta));*/
+
+
+        return view;
     }
 
+    public void Mensaje(String msg){ Toast.makeText(getActivity(), msg,Toast.LENGTH_SHORT).show();};
 }
--- app/src/main/java/com/example/denis/a2night/Perfil.java
+++ app/src/main/java/com/example/denis/a2night/Perfil.java
@@ -1,13 +1,17 @@
 package com.example.denis.a2night;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -28,12 +32,50 @@ public class Perfil extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_perfil, container, false);
 
-
+        OnclickDelButton(view.findViewById(R.id.editarPerfil));
+        OnclickDelButton(view.findViewById(R.id.seguidores));
+        OnclickDelButton(view.findViewById(R.id.editarMenu));
+        OnclickDelButton(view.findViewById(R.id.agregarPublicacion));
+        OnclickDelButton(view.findViewById(R.id.salir));
 
         return view;
-
     }
 
+    public void OnclickDelButton(View view) {
+        ImageView opciones = (ImageView)  view;
+        opciones.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // if(msg.equals("Texto")){Mensaje("Texto en el botón ");};
+                switch (v.getId()) {
 
+                    case R.id.editarPerfil:
+                        Mensaje("Editar");
+                        //FragmentTransaction transaction1 = getFragmentManager().beginTransaction();
+                        //transaction1.replace(R.id.scrollPerfilUsuario, new EditarPerfil()).commit();
+                        break;
 
+                    case R.id.seguidores:
+                        FragmentTransaction transaction2 = getFragmentManager().beginTransaction();
+                        transaction2.replace(R.id.contentPerfil, new Seguidores()).commit();
+                        break;
+
+                    case R.id.editarMenu:
+                        FragmentTransaction transaction3 = getFragmentManager().beginTransaction();
+                        transaction3.replace(R.id.scrollPerfilUsuario, new TabMenu()).commit();
+                        break;
+
+                    case R.id.agregarPublicacion:
+                        FragmentTransaction transaction4 = getFragmentManager().beginTransaction();
+                        transaction4.replace(R.id.scrollPerfilUsuario, new AgregarPublicacion()).commit();
+                        break;
+
+                    case R.id.salir:
+                        break;
+                    default:break; }// fin de casos
+            }// fin del onclick
+        });
+    }// fin de OnclickDelButton
+
+    public void Mensaje(String msg){ Toast.makeText(getActivity(), msg,Toast.LENGTH_SHORT).show();};
 }
